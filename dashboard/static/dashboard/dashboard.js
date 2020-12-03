9332fccbf169adea1c9ad2c1f896f3bae2b1958a
Nicolas Noé <nicolas.noe@inbo.be>
2020-12-03 11:57:33 +0100

    Separate Vue components

--- dashboard/static/dashboard/dashboard.js
+++ dashboard/static/dashboard/dashboard.js
@@ -1,3 +1,8 @@
-function toto() {
-    console.log("Bazar");
-}
+Vue.component('dashboard-map', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked1 me {{ count }} times.</button>'
+})
