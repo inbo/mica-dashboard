5c4135ed419059a32cacd1f12b458dae2e3cb872
Nicolas Noé <nicolas.noe@inbo.be>
2021-04-02 09:37:00 +0200

    Removed code (unused map types + whole occ load at first)

--- dashboard/static/dashboard/dashboard.js
+++ dashboard/static/dashboard/dashboard.js
@@ -37,7 +37,8 @@ Vue.component('occurrence-table-page', {
                </tbody>`
 });
 
-// The main table component (get all occurrence, manage table options, header and pagination)
+// The main table component (receive all occurrence, manage table options, header and pagination)
+// TODO: make it load data according to pagination
 Vue.component('dashboard-table', {
     props: {
         'occurrences': { // All occurrences, coming from the main component
@@ -112,22 +113,11 @@ Vue.component('dashboard-table', {
 // The main map
 Vue.component('dashboard-map', {
     props: {
-        'occurrences': Array,
-
         'minMaxUrl': String,
 
         'initialLat': Number,
         'initialLon': Number,
         'initialZoom': Number,
-
-        'heatmapBlur': {
-            type: Number,
-            default: 20
-        },
-        'heatmapRadius': {
-            type: Number,
-            default: 2
-        },
 
         'visibleLayer': String // "pointsLayer" | "clusterLayer" | "heatmapLayer"
     },
@@ -146,31 +136,6 @@ Vue.component('dashboard-map', {
                 this.setLayerVisibility(val);
             },
         },
-        heatmapBlur: {
-            handler: function (val) {
-                this.heatmapLayer.setBlur(val);
-            },
-        },
-        heatmapRadius: {
-            handler: function (val) {
-                this.heatmapLayer.setRadius(val);
-            },
-        },
-        occurrences: {
-            handler: function (val) {
-                this.vectorSource.clear(true);
-
-                var allFeatures = []
-                val.forEach(function (occ) {
-                    allFeatures.push(new ol.Feature({
-                        geometry: new ol.geom.Point(ol.proj.fromLonLat([occ.lon, occ.lat]))
-                    }))
-                });
-                this.vectorSource.addFeatures(allFeatures);
-
-            },
-            immediate: true
-        },
     },
     computed: {
         colorScale: function () {
@@ -185,21 +150,6 @@ Vue.component('dashboard-map', {
             return this.allLayers.filter(function (l) {
                 return l.get('dataLayer') == true;
             })
-        },
-        pointsLayer: function () {
-            return this.dataLayers.find(function (l) {
-                return (l.get('name') == 'pointsLayer')
-            });
-        },
-        clusterLayer: function () {
-            return this.dataLayers.find(function (l) {
-                return (l.get('name') == 'clusterLayer')
-            });
-        },
-        heatmapLayer: function () {
-            return this.dataLayers.find(function (l) {
-                return (l.get('name') == 'heatmapLayer')
-            });
         },
         vectorTilesLayer: function () {
             return this.dataLayers.find(function (l) {
@@ -216,58 +166,6 @@ Vue.component('dashboard-map', {
                     l.setVisible(false);
                 }
             })
-        },
-        createClusterLayer: function () {
-            var vm = this;
-
-            var clusterSource = new ol.source.Cluster({
-                distance: 40,
-                source: this.vectorSource,
-            });
-
-            var l = new ol.layer.Vector({
-                source: clusterSource,
-                visible: false,
-                style: function (feature) {
-                    var size = feature.get('features').length;
-                    var style = vm.clusterStyleCache[size];
-                    if (!style) {
-                        style = new ol.style.Style({
-                            image: new ol.style.Circle({
-                                radius: 15,
-                                stroke: new ol.style.Stroke({
-                                    color: '#fff',
-                                }),
-                                fill: new ol.style.Fill({
-                                    color: '#3399CC',
-                                }),
-                            }),
-                            text: new ol.style.Text({
-                                text: size.toString(),
-                                fill: new ol.style.Fill({
-                                    color: '#fff',
-                                }),
-                            }),
-                        });
-                        vm.clusterStyleCache[size] = style;
-                    }
-                    return style;
-                },
-            });
-            l.set('name', 'clusterLayer')
-            l.set('dataLayer', true);
-            return l;
-        },
-        createHeatmapLayer: function () {
-            var l = new ol.layer.Heatmap({
-                source: this.vectorSource,
-                visible: false,
-                blur: this.heatmapBlur,
-                radius: this.heatmapRadius,
-            });
-            l.set('name', 'heatmapLayer')
-            l.set('dataLayer', true);
-            return l;
         },
         createVectorTilesLayer: function () {
             var vm = this;
@@ -296,22 +194,6 @@ Vue.component('dashboard-map', {
             l.set('dataLayer', true);
             return l;
         },
-        createPointsLayer: function () {
-            var l = new ol.layer.WebGLPoints({
-                source: this.vectorSource,
-                visible: false,
-                style: {
-                    symbol: {
-                        symbolType: 'square',
-                        size: 10,
-                        color: 'rgba(255,0,0,0.1)'
-                    }
-                }
-            })
-            l.set('name', 'pointsLayer')
-            l.set('dataLayer', true);
-            return l;
-        },
         loadOccMinMax: function (zoomLevel) {
             var vm = this;
             $.ajax({
@@ -332,9 +214,6 @@ Vue.component('dashboard-map', {
             return new ol.Map({
                 layers: [
                     baseLayer,
-                    this.createClusterLayer(),
-                    this.createHeatmapLayer(),
-                    this.createPointsLayer(),
                     this.createVectorTilesLayer()
                 ],
                 view: new ol.View({
@@ -349,7 +228,6 @@ Vue.component('dashboard-map', {
         this.map = this.createMap();
         this.map.setTarget(this.$refs['map-root']); // Assign the map to div and display
         this.setLayerVisibility(this.visibleLayer);
-        this.$emit('finished-mounting');
     },
     template: '<div id="map" class="map" ref="map-root" style="height: 500px; width: 100%;"></div>'
 })
